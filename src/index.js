0b6f16f917ea35c6883337739e7089c4c11e89e8
hshine1226 <hshine1226@gmail.com>
2020-04-24 11:56:18 +0900

    기능: 클래스를 이용해서 body backgroundcolor 변경 기존에는 js에서 body.style.backgroundColor를 통해서 backgroundColor를 변경했는데, Style적인 요소들은 모두 CSS에서 처리하기 위해서, CSS에 클래스를 선언해서 처리했다.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,35 @@
 // <⚠️ /DONT DELETE THIS ⚠️>
 const color = ["#f1c40f", "#9b59b6", "#3498db"];
 const body = document.querySelector("body");
+const BIG_SCREEN = "bigScreen",
+  MIDIUM_SCREEN = "midiumScreen",
+  SMALL_SCREEN = "smallScreen";
 
-function handleInnerWidth(){
-    const intViewportWidth = window.innerWidth;
-    if (intViewportWidth > 1200) {
-        body.style.backgroundColor = color[0];
-    }
-    else if (intViewportWidth > 500){
-        body.style.backgroundColor = color[1];
-    }
-    else{
-        body.style.backgroundColor = color[2];
-    }
+function handleBodyColor() {
+  const width = window.innerWidth;
+  if (width > 1000) {
+    body.classList.remove(MIDIUM_SCREEN, SMALL_SCREEN);
+    body.classList.add(BIG_SCREEN);
+  } else if (width > 500 && width < 1000) {
+    body.classList.remove(SMALL_SCREEN, BIG_SCREEN);
+    body.classList.add(MIDIUM_SCREEN);
+  } else {
+    body.classList.remove(MIDIUM_SCREEN, BIG_SCREEN);
+    body.classList.add(SMALL_SCREEN);
+  }
 }
 
-window.addEventListener("resize", handleInnerWidth);
+/*function handleInnerWidth() {
+  const intViewportWidth = window.innerWidth;
+  if (intViewportWidth > 1200) {
+    body.style.backgroundColor = color[0];
+  } else if (intViewportWidth > 500) {
+    body.style.backgroundColor = color[1];
+  } else {
+    body.style.backgroundColor = color[2];
+  }
+}
+
+window.addEventListener("resize", handleInnerWidth);*/
+
+window.addEventListener("resize", handleBodyColor);
